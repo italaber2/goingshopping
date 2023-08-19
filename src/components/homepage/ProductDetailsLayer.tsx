363ab288e086b44eb91ea9a2feff3782f4885cdc
Ian Talaber <ian.talaber@accenture.com>
2023-08-19 12:45:41 +0200

    minor fixes

--- src/components/homepage/ProductDetailsLayer.tsx
+++ src/components/homepage/ProductDetailsLayer.tsx
@@ -8,7 +8,7 @@ function ProductDetailsLayer({ product, onClose }: ProductDetailsProps) {
     <div className="layer">
       <div className="layer-content">
         <h2>{product.name}</h2>
-        <img src={product.picture} alt={product.name} />{" "}
+        <img src={product.picture} alt={product.name} />
         <p>{product.description}</p>
         <button className="button" onClick={onClose}>
           Close
