68957d861ec73b9eae813112b04cbcaef2e35f45
Ian Talaber <ian.talaber@accenture.com>
2023-08-19 16:41:22 +0200

    minor changes

--- src/components/ViewBasket.tsx
+++ src/components/ViewBasket.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import ViewBasketLayer from "./ViewBasketLayer";
 
 interface BasketProps {
   cartItemsCount: number;
---
+++ src/data/inventory.ts
@@ -0,0 +1,58 @@
+export const inventory = [
+  { name: "Apple", price: 10, inventory: 1 },
+  { name: "Banana", price: 20, inventory: 1 },
+  { name: "Cherry", price: 30, inventory: 2 },
+  { name: "Date", price: 40, inventory: 3 },
+  { name: "Elderberry", price: 50, inventory: 5 },
+  { name: "Fig", price: 60, inventory: 8 },
+  { name: "Grape", price: 60, inventory: 13 },
+  { name: "Honeydew", price: 60, inventory: 21 },
+];
+
+export const inventoryProductDetails = [
+  {
+    name: "Apple",
+    picture: require("../images/apple.jpg"),
+    description:
+      "The original forbidden fruit, proving that even temptation can be disappointingly ordinary.",
+  },
+  {
+    name: "Banana",
+    picture: require("../images/banana.jpg"),
+    description:
+      "Nature's attempt at a practical joke, with a flavor and texture that defy all standards of enjoyable eating.",
+  },
+  {
+    name: "Cherry",
+    picture: require("../images/cherry.jpg"),
+    description: "Tiny bursts of disappointment.",
+  },
+  {
+    name: "Date",
+    picture: require("../images/date.jpg"),
+    description: "Even the best ones can still go bad.",
+  },
+  {
+    name: "Elderberry",
+    picture: require("../images/elderberry.jpg"),
+    description: "Meh.",
+  },
+  {
+    name: "Fig",
+    picture: require("../images/fig.jpg"),
+    description:
+      "The fruit that never quite 'fig'-ured out how to win my taste buds over.",
+  },
+  {
+    name: "Grape",
+    picture: require("../images/grape.jpg"),
+    description:
+      "Why did the grape stop in the middle of the road? Because it ran out of juice!",
+  },
+  {
+    name: "Honeydew",
+    picture: require("../images/honeydew.jpg"),
+    description:
+      "Honeydew melons are known as 'winter melons' in some parts of the world because they can thrive in cooler temperatures, making them available even during colder months.",
+  },
+];
